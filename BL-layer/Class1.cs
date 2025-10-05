7dba81b04bf06ab05e620607d2d3b84442390fc5
Muditha Weerarathna <mmweerarathna123@gmail.com>
2025-10-06 00:02:40 +0530

    initial commit

---
+++ BL-layer/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace BL_layer
+{
+    //This layer handles all the business logics within the application
+    public class Class1
+    {
+
+    }
+}
---
+++ DAL-layer/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace DAL_layer
+{
+    //this layer handles all the CRUD operations, queries that runs to get data from the database
+    public class Class1
+    {
+
+    }
+}
